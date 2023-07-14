1bd469bb85d2263fe9f83780b9c690be8d797c73
talha4t <mahmudtalha80@gmail.com>
2023-07-14 18:44:27 +0600

    Phase 01 Remastered Solve one problem

---
+++ 1-Practice/Contest-1-Vjudge/A-Multiplication-1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int 
+
+bool solve() {
+    
+    int a, b; cin >> a >> b;
+
+    cout << a * b << '\n';
+    
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+
+/*
+    author:  talha4t
+    github.com/talha4t (Talha Mahmud)
+    Friday, July 14, 2023 | 18:42:37 (UTC +06:00) Dhaka
+*/
